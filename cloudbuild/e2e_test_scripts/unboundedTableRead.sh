1c21c725863aee77eed49ee7c842a8088639b2df
Prashasti Agarwal <144427971+prashastia@users.noreply.github.com>
2023-12-05 12:42:48 +0530

    Update unboundedTableRead.sh

--- cloudbuild/e2e_test_scripts/unboundedTableRead.sh
+++ cloudbuild/e2e_test_scripts/unboundedTableRead.sh
@@ -30,7 +30,7 @@ echo JOB ID: "$JOB_ID"
 gcloud dataproc jobs submit flink --id "$JOB_ID" --jar=$JAR_LOCATION --cluster=$CLUSTER_NAME --region=$REGION --async -- --gcp-project $ARG_PROJECT --bq-dataset $ARG_DATASET --bq-table $ARG_TABLE_UNBOUNDED_TABLE --streaming --agg-prop name --ts-prop ts --refresh-interval $REFRESH_INTERVAL
 
 # Dynamically adding the data. This is timed 2.5 min wait for read and 5 min refresh time.
-python cloudbuild/python_scripts/insertDynamicPartitions.py -- --now_timestamp="$now" --arg_project=$ARG_PROJECT --arg_dataset=$ARG_DATASET --arg_table=$ARG_TABLE_UNBOUNDED_TABLE --refresh_interval=$REFRESH_INTERVAL
+python3 cloudbuild/python_scripts/insertDynamicPartitions.py -- --now_timestamp="$now" --arg_project=$ARG_PROJECT --arg_dataset=$ARG_DATASET --arg_table=$ARG_TABLE_UNBOUNDED_TABLE --refresh_interval=$REFRESH_INTERVAL
 
 #Kill the dataproc job
 gcloud dataproc jobs kill "$JOB_ID" --region=$REGION
