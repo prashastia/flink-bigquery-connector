c0a89a46ce91b743d801b14458281fd124abee2f
Prashasti Agarwal <prashastia@google.com>
2024-01-11 10:07:31 +0530

    Adds script to create a partitioned table, also modifies unbounded read script to execute the same.

--- cloudbuild/nightly/scripts/unbounded_table_read.sh
+++ cloudbuild/nightly/scripts/unbounded_table_read.sh
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Run python script to create the partitioned table and insert values.
+python3 cloudbuild/nightly/scripts/python-scripts/create_partitioned_table.py -- --project_name "$PROJECT_NAME" --dataset_name "$DATASET_NAME" --table_name "$TABLE_NAME"
+
 # Running this job async to make sure it exits so that dynamic data can be added
 gcloud dataproc jobs submit flink --id "$JOB_ID" --jar="$GCS_JAR_LOCATION" --cluster="$CLUSTER_NAME" --region="$REGION" --async -- --gcp-project "$PROJECT_NAME" --bq-dataset "$DATASET_NAME" --bq-table "$TABLE_NAME" --mode unbounded --agg-prop "$AGG_PROP_NAME" --ts-prop "$TS_PROP_NAME" --partition-discovery-interval "$PARTITION_DISCOVERY_INTERVAL"
 
