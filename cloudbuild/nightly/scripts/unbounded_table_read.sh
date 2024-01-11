201b4929878c858d73600373ae10aec30926e8f6
Prashasti Agarwal <prashastia@google.com>
2024-01-11 21:50:47 +0530

    Modifies unbounded read script to create the copy of table to make the tests idempotent.

--- cloudbuild/nightly/scripts/unbounded_table_read.sh
+++ cloudbuild/nightly/scripts/unbounded_table_read.sh
@@ -14,8 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Run python script to create the partitioned table and insert values.
-python3 cloudbuild/nightly/scripts/python-scripts/create_partitioned_table.py -- --project_name "$PROJECT_NAME" --dataset_name "$DATASET_NAME" --table_name "$TABLE_NAME"
+# Get the timestamp to annotate the copy of the partitioned table.
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Copy the table
+bq cp -f "$DATASET_NAME"."$TABLE_NAME" "$DATASET_NAME"."$TABLE_NAME"_"$timestamp"
+
+# Change the table name for all future uses.
+TABLE_NAME="$TABLE_NAME"_"$timestamp"
+
+# Set the expiration time to 1 hour.
+bq update --expiration 3600 "$DATASET_NAME"."$TABLE_NAME"
 
 # Running this job async to make sure it exits so that dynamic data can be added
 gcloud dataproc jobs submit flink --id "$JOB_ID" --jar="$GCS_JAR_LOCATION" --cluster="$CLUSTER_NAME" --region="$REGION" --async -- --gcp-project "$PROJECT_NAME" --bq-dataset "$DATASET_NAME" --bq-table "$TABLE_NAME" --mode unbounded --agg-prop "$AGG_PROP_NAME" --ts-prop "$TS_PROP_NAME" --partition-discovery-interval "$PARTITION_DISCOVERY_INTERVAL"
